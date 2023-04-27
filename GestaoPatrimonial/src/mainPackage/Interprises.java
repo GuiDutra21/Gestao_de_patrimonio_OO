0ea4296bdc87963b10883e98510c8bb1acd458af
Guilherme <guilhermesilvadutra2004@gmail.com>
2023-04-27 17:00:37 -0300

    primeiro commit

---
+++ GestaoPatrimonial/src/mainPackage/Interprises.java
@@ -0,0 +1,30 @@
+package mainPackage;
+
+public abstract class Interprises {
+	// ATRIBUTES
+	private String name;
+	private Adress adress;
+
+	// GETTERS AND SETTERS
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setAdress(Adress adress) {
+		this.adress = adress;
+	}
+
+	public Adress getAdress() {
+		return adress;
+	}
+	
+	public void setHola() {
+		//Hollaaaaaaaaa
+	}
+
+}
