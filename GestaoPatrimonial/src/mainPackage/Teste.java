bb12bdef02a5f0c357bd251354ba3733e00349b0
Guilherme <guilhermesilvadutra2004@gmail.com>
2023-04-29 18:34:37 -0300

    comitt 29/4 18:34

--- GestaoPatrimonial/src/mainPackage/Teste.java
+++ GestaoPatrimonial/src/mainPackage/Teste.java
@@ -1,8 +1,7 @@
 package mainPackage;
-
+import java.util.Scanner;
 public class Teste {
 	public static void main(String[] args) {
-		
 		
 		Adress a = new Adress("Brasil","Alagoas","Maceio","AzulL",23);
 		
@@ -14,8 +13,51 @@ public class Teste {
 		
 		System.out.println(c.getFilials().get(1).getName());
 		
+		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Seja bem vindo ao Menu!");
+		System.out.println("------------------------");
+		System.out.print("Informe o nome da sua companinha\nName : ");
+		String interpriseName = input.nextLine();
+		System.out.println("Informe o endereco da sua empresa");
+		System.out.print("Pais : ");
+		String interpriseCountry = input.nextLine();
+		System.out.print("Estado : ");
+		String interpriseState = input.nextLine();
+		System.out.print("Cidade : ");
+		String interpriseCity = input.nextLine();
+		System.out.print("Rua : ");
+		String interpriseStreet = input.nextLine();
+		System.out.print("Numero : ");
+		int intepriseNumber = input.nextInt();
 		
+		Adress a1 = new Adress(interpriseCountry, interpriseState, interpriseCity, interpriseStreet, intepriseNumber );
+		Company company1 = new Company(interpriseName, a1);
 		
+		while(true)
+		{
+			System.out.println("Digite a opcao que voce deseja :");
+			System.out.println("1 - alterar os dados da empresa\n2 - gerenciar uma filial\n3 - gerenciar patrimonios");
+			int option1 = input.nextInt();
+			if(option1 != 1 && option1 != 2 && option1 != 3)
+			{
+				break;
+			}
+			
+			if(option1 == 1)
+			{	
+				System.out.println("Voce selecionou a primeira opcao !");
+				System.out.println("Digite a opcao que voce deseja :");
+				System.out.println("1 - alterar o nome\n 2 - alterar o endereco");
+				int option2 = input.nextInt();
+				if(option2 != 1 && option2 != 2){
+					break;
+				}
+				
+				if(option2 == 1){
+					
+				}
+			}
+		}		
 	}
 }
